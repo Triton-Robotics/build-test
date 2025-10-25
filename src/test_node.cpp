9bc9a46336d16f7ffd92e40e87b4464e73319973
Kane Li <122249106+Li-Kane@users.noreply.github.com>
2025-10-25 14:01:10 -0700

    make sure colcon test works

--- src/test_node.cpp
+++ src/test_node.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
-int main(int argc, char** argv)
+int main(int argc, char ** argv)
 {
   (void)argc;
   (void)argv;
-  
+
   std::cout << "test successful!" << std::endl;
-  
+
   return 0;
 }
